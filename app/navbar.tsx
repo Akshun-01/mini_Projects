a55d52c51306309581f1e5f7ed60b3cdf4156cb1
Akshun-01 <akshunkuthiala2002@gmail.com>
2023-12-20 18:06:59 +0530

    initial commit

---
+++ app/navbar.tsx
@@ -0,0 +1,14 @@
+// --- List of websites you want to add to miniProject ---
+// loading screen
+// following eyes
+// neon-btn
+// raining code
+// threejs room
+// pomodoro
+// globe.info
+// ip address
+// 3dNavbar?
+// audioVisualizer
+// bouncing-physics canvas
+// tic-tac-toe game
+// angelaYuPractice ?
